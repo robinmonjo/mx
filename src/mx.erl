63283ed2fc57c2b1eab8106f8579d529748ac089
Robin Monjo <robin.monjo@applidget.com>
2016-04-12 13:30:27 +0200

    basic mongodb integration

--- src/mx.erl
+++ src/mx.erl
@@ -9,27 +9,47 @@
 
 %% escript Entry point
 main(_Args) ->
-  Nb = 10,
-  spawn_workers(Nb),
-  loop(Nb).
+  required_application_do(start),
+  
+  %% config
+  Nb = 1,
+  Database = <<"mobi_checkin_development">>,
+  Collection = <<"guests">>,
+
+  case mc_worker_api:connect([{database, Database}]) of
+    {ok, Connection} -> 
+      Nb_docs = mc_worker_api:count(Connection, Collection, {}),
+      io:format("Starting export on ~p/~p with ~p workers (~p documents)~n", [Database, Collection, Nb, Nb_docs]),
+      
+      spawn_workers(Nb, Connection, Collection),
+      loop(Nb, Connection);
+    {error, Reason} -> io:format("unable to connect to ~p: ~p~n", [Database, Reason])
+  end.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
-spawn_workers(0) ->
+spawn_workers(0, _, _) ->
   ok;
-spawn_workers(Nb) ->
-  spawn(mx_worker, work, [self(), "some args"]),
-  spawn_workers(Nb - 1).
+spawn_workers(Nb, Connection, Collection) ->
+  spawn(mx_worker, work, [self(), Connection, Collection, 0, 1]),
+  spawn_workers(Nb - 1, Connection, Collection).
   
-loop(0) ->
+loop(0, Connection) ->
+  mc_worker_api:disconnect(Connection),
+  required_application_do(stop),
   erlang:halt(0);
-loop(Nb) ->
+loop(Nb, Connection) ->
   receive
     {worker_done, Pid} ->
       io:format("~p finished~n", [Pid]),
-      loop(Nb - 1);
+      loop(Nb - 1, Connection);
     _ ->
       io:format("Received an unknown message~n"),
-      loop(Nb)
+      loop(Nb, Connection)
   end.
+  
+required_application_do(Action) ->
+  apply(application, Action, [bson]),
+  apply(application, Action, [crypto]),
+  apply(application, Action, [mongodb]).
--- src/mx_worker.erl
+++ src/mx_worker.erl
@@ -1,10 +1,31 @@
 -module(mx_worker).
 
--export([work/2]).
+-export([work/5]).
 
-work(Spawner, Args) ->
-  io:format("~p starting with args: ~p~n", [self(), Args]),
+work(Spawner, Connection, Collection, Skip, Limit) ->
+  Cursor = mc_worker_api:find(Connection, Collection, {}, #{skip => Skip}),
+  Data = export_fields(Cursor, Limit),
+  %% TODO here I've got a list of csv rows, in reverse orders
+  io:format("~p - data: ~p~n", [self(), Data]),
   Spawner ! {worker_done, self()}.
+
+export_fields(Cursor, Limit) ->
+  export_fields(Cursor, Limit, []).
+  
+export_fields(Cursor, 0, Acc) ->
+  mc_cursor:close(Cursor),
+  Acc;
+export_fields(Cursor, Limit, Acc) ->
+  { Data } = mc_cursor:next(Cursor),
+  F = fun(_Key, Value, FieldsAcc) ->
+      [Value | FieldsAcc]
+    end,
+  Fields = maps:fold(F, [], Data),
+  NewAcc = [Fields | Acc],
+  export_fields(Cursor, Limit - 1, NewAcc).
+  
+  
+  
   
   
   
